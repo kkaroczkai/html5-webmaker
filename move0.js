765211999e675d9cab3dcba5e50b320642af1329
jkqxl2 <krisztian.karoczkai@sztaki.hu>
2015-08-12 17:55:14 +0200

    pacman

---
+++ move0.js
@@ -0,0 +1,7 @@
+            function move0(pname,px,py,pmap){
+                $("body").trigger({
+			type: "refreshmap",
+			name: pname,
+			walk: Math.floor(Math.random()*4)
+		});
+            }
